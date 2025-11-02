11b5461cec8baeb3e989e72d5a078f61b1a4ebba
Raushan Kumar <147327480+raushan0785@users.noreply.github.com>
2025-11-02 20:10:15 +0530

    Update MongoDB connection string to use Atlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/codeial_development');
+mongoose.connect('mongodb+srv://raushan07851:m7j3BbgluIgN5VjI@cluster0.2sluirf.mongodb.net/codeial?retryWrites=true&w=majority');
+
 
 const db = mongoose.connection;
 
